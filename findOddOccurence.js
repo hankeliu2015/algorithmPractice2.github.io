040cbd6e1282a5dcc615f7fa3d89f5f2bfa7a5d4
github username <github email address>
2019-09-22 23:49:03 -0400

    add findOddOccurence

---
+++ findOddOccurence.js
@@ -0,0 +1,21 @@
+function findOddOccurence(arr) {
+  var arrHash = {};
+  for (var i = 0; i < arr.lengthl; i++) {
+    if (arrHash[arr[i]] !== undefined) {
+      arrHash[arr[i]] ++
+    } else {
+      arrHash[arr[i]] = 1
+    }
+  }
+  for ( var key in arrHash) {
+    if (arrHash[key] % 2 !== 0) {
+      debugger
+      return key;
+    }
+  }
+}
+  // {1:2, 2: 4, 4:3}
+const input = [1,2,4,2,2,4,1,4];
+console.log(findOddOccurence(input))
+console.log("testing is running")
+// will return 4
