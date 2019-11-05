f5ec58ae70a865927d2abef1743ab61d5e3eb619
github username <github email address>
2019-11-04 20:56:53 -0500

    use sort function replace compare conditions

--- sumAll.js
+++ sumAll.js
@@ -1,19 +1,23 @@
 function sumAll(arr) {
 
-  let a, b
-  if (arr[0] < arr[1]) {
-    a = arr[0]
-    b = arr[1]
-  } else if (arr[0] > arr[1]) {
-    a = arr[1]
-    b = arr[0]
-  }
+  // let a, b
+  // if (arr[0] < arr[1]) {
+  //   a = arr[0]
+  //   b = arr[1]
+  // } else if (arr[0] > arr[1]) {
+  //   a = arr[1]
+  //   b = arr[0]
+  // }
+  // let sum = 0
+  // for (var i = 0; i <= b - a; i++) {
+    //   sum = sum + (a + i)
+    // }
 
-  let newArr = arr.sort(a-b)
-console.log(newArr)
+  let newArr = arr.sort(el => a-b)
+  // console.log(newArr)
   let sum = 0
-  for (var i = 0; i <= b - a; i++) {
-    sum = sum + (a + i)
+  for (var i = 0; i <= newArr[1] - newArr[0]; i++) {
+    sum = sum + (newArr[0] + i)
   }
   return sum;
 }
