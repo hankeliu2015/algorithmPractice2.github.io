a511faaaeea82600b9ccc1ba371ed7c6bbc6465a
github username <github email address>
2019-11-10 17:32:04 -0500

    format update

--- stairCase.js
+++ stairCase.js
@@ -1,27 +1,21 @@
-// loop n times. concate # to a declared array.
-// first element contains one #, the last element contains n #s.
-// return the array.
-// need a way to creat empty space. use an loop n-1 times inside.
+// the array inside array methods has some issue, even it worked on my node.
+// hackerRank testing environment might have issue on that.
 
 function stairCase(n) {
-  let arr = new Array();
 
   for (let i = 0; i < n ; i++) {
 
     for (let j = 0; j < n-i-1; j++) {
-      // arr[i].push('');
-      // arr[i] = arr[i].concat(' ');
       process.stdout.write(" ");
     }
 
     for (let k = 0; k < i+1; k++) {
-      // arr[i] = arr[i].concat("#");
       process.stdout.write("#");
     }
+
     process.stdout.write("\n");
-    // console.log(arr[i]);
   }
 
 }
 
-stairCase(10);
+// stairCase(10);
