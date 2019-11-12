7baf591912a78680eed4af56f06ade9e4d716700
github username <github email address>
2019-11-11 23:39:19 -0500

    add solutin syntax

--- whatIsInAName.js
+++ whatIsInAName.js
@@ -1,16 +1,18 @@
-// loop the array collection
-// use for in loop to iterate each object and compare with source
+// my code:
+// filter out the first argument object array meet the condition.
+// inside the filter, use for in loop for 1st argument.
+// within the above for in loop, declare another for in loop for 2nd argument,
+// if the 2nd argument not has any keys from frist argument, return false,
+// if the 2nd argument has the same object but value is not the same as the 1st one, return false. end for in loop.
 
-
+// set a condition, if the 1st and 2nd has the same key, and value are the same, also 2nd argument has less keys than the 1st argument, return true.
 
 function whatIsInAName(collection, source) {
-  let foundItems = collection.filter(obj => {
-
+  let matchedArray = collection.filter(obj => {
     for (const key in obj ) {
-
       for (const k in source) {
-        if ( !obj.hasOwnProperty(k)) {
-          return false
+        if ( !obj.hasOwnProperty(k) || source[k] !== obj[k]) {
+          return false;
         }
       }
 
@@ -19,10 +21,8 @@ function whatIsInAName(collection, source) {
       }
     }
   })
-
-  console.log(foundItems);
-  return foundItems;
-
+  console.log(matchedArray);
+  return matchedArray;
 }
 
 // whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
@@ -31,4 +31,4 @@ function whatIsInAName(collection, source) {
 //
 // whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
 
-whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3})
+// whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3})
