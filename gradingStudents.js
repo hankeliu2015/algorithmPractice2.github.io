8aa34e71a961ae516f332bf492c33c79ec98448a
github username <github email address>
2019-11-13 23:50:06 -0500

    declare gradingStudent

---
+++ gradingStudents.js
@@ -0,0 +1,18 @@
+// split the grade to single digits.
+// the single digit minus 8 less than 3 or minus 5 greater than 3.
+// make a condition for grade 100.
+
+function gradingStudents(grades) {
+  grades.forEach( grade => {
+    gradeString = grade.toString();
+    if (grade < 40) {
+      console.log(grade);
+    }else if () {
+
+    }
+  })
+
+
+}
+
+gradingStudents([73,67,38,33])
