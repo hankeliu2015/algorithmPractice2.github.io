35310acf9b44a479e9b51cb58da9615b85c59a7b
github username <github email address>
2019-11-17 14:01:13 -0500

    pigLatin file created

---
+++ pigLatin.js
@@ -0,0 +1,21 @@
+//1st condition if first char is a vow, return way.
+// if not start as a vowels, split the conso chars and concat 'ay' push to the end of the stringPermutation
+// seperate the conso chars with the rest of string.
+
+
+
+function translatePigLatin(str) {
+  console.log(str);
+  if (str.match(/^[aeiouAEIOU]\w+/)) {
+    return str.toLowerCase().concat('way')
+  }else {
+    strArray = str.split(/(?=[aeiouAEIOU])/)
+  }
+
+  newStr = strArray[1].concat(str[0]).concat('ay')
+
+  return newStr;
+}
+
+console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("consonant"));
