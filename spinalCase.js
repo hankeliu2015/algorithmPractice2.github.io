c6d3ce08e520a87b3d70ffd2774bde7e1cc05ba9
github username <github email address>
2019-11-17 12:19:22 -0500

    testing with replace method passed

--- spinalCase.js
+++ spinalCase.js
@@ -1,5 +1,5 @@
 
-// my solutions:
+// my solutions - split()
 // spit string with space. uppercase char or underscore into array
 // concate the array back with dash and lower case each element.
 // remove the first string, which is a dash.
@@ -15,23 +15,24 @@
 //   return spinalStr.slice(1);
 // }
 
-//solution 1
-// replace the string's uppercase with lowerse with space.
-// set the patter of space and underscore with g option
-// reture the string, replaced pattern with dash, set to lowercase.
+//solution 1 - replace()
 
-function spinalCase(str) {
-  let regex = /\s+|_+/g;
-  str = str.replace(/([a-z])([A-Z])/, "$1 $2");
-  console.log(str);
-  return str.replace(regex, '-').toLowerCase();
+// use a variable to store the regular experssion space or underscore.
+//use replace to location the pattern at lower-upper case, change it to lower-space-uppercase. parenthesized substring matches is used and space is set by $1 $2
+// again use replace to location the pattern stored in varaible, replaced with dash.
 
-}
-
-
-
-console.log(spinalCase('This Is Spinal Tap'));
-console.log(spinalCase("thisIsSpinalTap"));
-console.log(spinalCase("The_Andy_Griffith_Show"));
-console.log(spinalCase("Teletubbies say Eh-oh"));
-console.log(spinalCase("AllThe-small Things"));
+// function spinalCase(str) {
+//   let regex = /\s+|_+/g;
+//   str = str.replace(/([a-z])([A-Z])/, "$1 $2");
+//   console.log(str);
+//   return str.replace(regex, '-').toLowerCase();
+//
+// }
+//
+//
+//
+// console.log(spinalCase('This Is Spinal Tap'));
+// console.log(spinalCase("thisIsSpinalTap"));
+// console.log(spinalCase("The_Andy_Griffith_Show"));
+// console.log(spinalCase("Teletubbies say Eh-oh"));
+// console.log(spinalCase("AllThe-small Things"));
