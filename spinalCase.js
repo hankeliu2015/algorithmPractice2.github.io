e7d45f9ddc00de220878bff7bb47e0039077de77
github username <github email address>
2019-11-16 21:05:03 -0500

    solution 1 testing works

--- spinalCase.js
+++ spinalCase.js
@@ -1,18 +1,34 @@
-// spit string with space. uppercase char or any not char
-// concatenate with -
 
+// my solutions:
+// spit string with space. uppercase char or underscore into array
+// concate the array back with dash and lower case each element.
+// remove the first string, which is a dash.
+
+// function spinalCase(str) {
+//   let strArray = str.split(/(?=[A-Z])|[\s+]|[_+]/);
+//   let spinalStr = '';
+//
+//   strArray.forEach(el => {
+//     spinalStr = spinalStr.concat(`-${el.toLowerCase()}`)
+//   })
+//
+//   return spinalStr.slice(1);
+// }
+
+//solution 1
+// replace the string's uppercase with lowerse with space.
+// set the patter of space and underscore with g option
+// reture the string, replaced pattern with dash, set to lowercase.
 
 function spinalCase(str) {
-  let strArray = str.split(/(?=[A-Z])|\s|[_]/);
-  let spinalStr = '';
+  let regex = /\s+|_+/g;
+  str = str.replace(/([a-z])([A-Z])/, "$1 $2");
+  console.log(str);
+  return str.replace(regex, '-').toLowerCase();
 
-  strArray.forEach(el => {
-    spinalStr = spinalStr.concat(`-${el.toLowerCase()}`)
-    // console.log(el, spinalStr)
-  })
-
-  return spinalStr.slice(1);
 }
+
+
 
 console.log(spinalCase('This Is Spinal Tap'));
 console.log(spinalCase("thisIsSpinalTap"));
