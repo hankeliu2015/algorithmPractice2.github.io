0d2bb61dd79f696dc530fac066c421f7d7783fdd
liuh <hanke.liu@gmail.com>
2019-11-17 20:06:19 -0500

    testing new regular expression

--- gradingStudents.js
+++ gradingStudents.js
@@ -44,15 +44,15 @@
 //     return finalGrades;
 // }
 
-function gradingStudents(grades){
-    return grades.map((n) => {
-        let diff = 5 - (n % 5);
-        if(diff < 3 && n >= 38) {
-            n += diff;
-        }
-
-        return n;
-    })
-}
-
-console.log(gradingStudents([4,73,67,38,33]));
+// function gradingStudents(grades){
+//     return grades.map((n) => {
+//         let diff = 5 - (n % 5);
+//         if(diff < 3 && n >= 38) {
+//             n += diff;
+//         }
+//
+//         return n;
+//     })
+// }
+//
+// console.log(gradingStudents([4,73,67,38,33]));
--- pigLatin.js
+++ pigLatin.js
@@ -2,20 +2,29 @@
 // if not start as a vowels, split the conso chars and concat 'ay' push to the end of the stringPermutation
 // seperate the conso chars with the rest of string.
 
-
-
 function translatePigLatin(str) {
   console.log(str);
+
+  let str1, str2;
+
   if (str.match(/^[aeiouAEIOU]\w+/)) {
     return str.toLowerCase().concat('way')
   } else {
     strArray = str.split(/(?=[aeiouAEIOU])/)
+    str1 = strArray[0];
+    str2 = strArray.slice(1).join('');
   }
 
-  newStr = strArray[1].concat(str[0]).concat('ay')
+  newStr = str2.concat(str1).concat('ay')
 
   return newStr;
 }
 
 console.log(translatePigLatin("algorithm"));
 console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("glove"));
+console.log(translatePigLatin("california"));
+console.log(translatePigLatin("paragraphs"));
+console.log(translatePigLatin("glove"));
+console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("schwartz"));
