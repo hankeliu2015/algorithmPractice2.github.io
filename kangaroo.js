eb19529155d0c9639ad470ce2553a4fd3b5ff4df
github username <github email address>
2019-11-21 12:07:33 -0500

    refactor while loop and eleminated 2 variables

--- kangaroo.js
+++ kangaroo.js
@@ -2,54 +2,54 @@
 // the slower left behind faster one, return false
 // faster on ahead of slower one, return false
 
-// function kangaroo(x1, v1, x2, v2) {
-//   if ( x1 >= x2 && v1 < v2) {
-//     return "NO";
-//   } else if ( x1 === x2 || x1 + v1 === x2 + v2) {
-//     return "YES";
-//   }
-//
-//   let vX1=v1;
-//   let vX2=v2;
-//
-//   while ( x1 + vX1 !== x2 + vX2 ) {
-//     vX1 = vX1 + v1;
-//     vX2 = vX2 + v2;
-//
-//     if (v1 <= v2 && x1 + vX1 < x2 + vX2  ) {
-//       return 'NO';
-//     } else if (v1 >= v2 && x1 + vX1 > x2 + vX2 ) {
-//       return 'NO';
-//     }
-//   }
-//
-//   if (x1 + vX1 === x2 + vX2) {
-//     return 'YES';
-//   }
-//
-// }
-//
-// console.log(kangaroo(0,3,4,2))
-// console.log(kangaroo(0,2,5,3))
+function kangaroo(x1, v1, x2, v2) {
+  if ( x1 >= x2 && v1 < v2) {
+    return "NO";
+  } else if ( x1 === x2 || x1 + v1 === x2 + v2) {
+    return "YES";
+  }
 
-// solution using difference ==0 ; need to test it.
-//         var difference=0;
+  while ( x1 !== x2  ) {
+    // console.log(x1, x2)
+    x1+=v1;
+    x2+=v2;
+
+    if (v1 <= v2 && x1 < x2) {
+      return 'NO';
+    } else if (v1 >= v2 && x1 > x2) {
+      return 'NO';
+    }
+  }
+
+  if (x1 === x2) {
+    return 'YES';
+  }
+}
+
+console.log(kangaroo(0,3,4,2))
+console.log(kangaroo(0,2,5,3))
+
+//// solution using difference ==0
+
+// function kangaroo(x1, v1, x2, v2) {
+//   var difference=0;
 //
-//        var calculate=function(){
-//            if((x1<x2)&&((v1<v2)||(v1==v2))){
-//          return "NO"
+//   var calculate=function(){
+//    if((x1<x2)&&((v1<v2)||(v1==v2))){
+//      return "NO"
 //      }
-//            else{
-// while(Math.sign(difference)!=-1){
-//     x1+=v1;
-//     x2+=v2;
-//     difference=x2-x1;
-//     if(difference==0){
-//         return "YES"
+//    else{
+//      while(Math.sign(difference)!=-1){
+//       x1+=v1;
+//       x2+=v2;
+//       difference=x2-x1;
+//       if(difference==0){
+//           return "YES"
+//         }
+//       }
+//       return "NO"
 //     }
-// }
-//         return "NO"
-//            }
-//        }();
+//   }();
+//
 //     return calculate;
 // }
