a4527874bcec85a7867f3360add13623d7b9d376
github username <github email address>
2019-11-23 23:27:43 -0500

    all test passed

--- getTotalX.js
+++ getTotalX.js
@@ -1,58 +1,38 @@
-// sort b's array.
-
-// iterate the sorted b array. Find all factors.
+// // sort both arrays.
+// // find those number can be divided by those between the Max of a and min of b, include Mac and min.
+// //use map and reduce to get all the true return element to single true
+// // push the number to new array.
+// // filter out the new array element is not a factor of any elements in b
 //
-// find the factor for each element from min to max.
-// the smallest element is the largetst factor for sortedB.
+// function getTotalX(a, b) {
+//   let sortedB = b.sort((a,b) => a - b);
+//   let sortedA = a.sort((a,b) => a - b);
+//   let sortedBFactors = [];
+//   let finalFactors;
 //
-// all b's common factors could divide a's elements.
-
-function getTotalX(a, b) {
-  let sortedB = b.sort((a,b) => a - b);
-  let sortedA = a.sort((a,b) => a - b);
-  let sortedBFactors = [];
-  let finalFactors;
-
-  // another soltion: the least in b will be the iterated range.
-  // loop range set the min element value from b. find all the factors for a.
-  // elimiate above from not factors for b.
-  // find those factors between the Max of a and min of b, include both of them.
-
-  for (let j = sortedA[a.length-1]; j <= sortedB[0]; j++) {
-
-    for (let i = 0; i < a.length; i++) {
-      if (j%a[i] ===0) {
-        sortedBFactors.push(j);     // find a away to push one j.
-      }
-    }
-  }
-
-  // console.log(sortedBFactors)
-
-  sortedB.forEach(el => {
-    sortedBFactors = sortedBFactors.filter(factor => el%factor === 0)
-  })
-
-
-  finalFactors = sortedBFactors.filter((v,i) => sortedBFactors.indexOf(v) ===i);
-  console.log(finalFactors)
-  return finalFactors.length;
-
-
-  // filter factors elements can be divided by a's element.
-  // finalFactors = sortedBFactors.filter(el => {
-  //   for (let i = 0; i < a.length; i++) {
-  //     if (el%a[i]!==0) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // })
-
-  // console.log(sortedBFactors, finalFactors)
-  // return finalFactors.length;
-}
-
-console.log(getTotalX([2,4], [16,32,96]))
-console.log(getTotalX([3,4], [24, 48]))
-// getTotalX([2,6], [24,36])
+//
+//   for (let j = sortedA[a.length-1]; j <= sortedB[0]; j++) {
+//
+//   let match = sortedA.map(el => {
+//       return j%el === 0
+//     }).reduce(function(a,b) {
+//       return a && b;
+//     })
+//
+//     if (match) {
+//       sortedBFactors.push(j);
+//     }
+//   }
+//
+//   sortedB.forEach(el => {
+//     sortedBFactors = sortedBFactors.filter(factor => el%factor === 0)
+//   })
+//
+//   console.log(sortedBFactors)
+//   return sortedBFactors.length;
+//
+// }
+//
+// console.log(getTotalX([2,4], [16,32,96]))
+// console.log(getTotalX([3,4], [24, 48]))
+// // getTotalX([2,6], [24,36])
