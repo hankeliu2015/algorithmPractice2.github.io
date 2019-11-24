5a906db3c3e1177b79c70f836942965416f593d4
github username <github email address>
2019-11-23 22:56:14 -0500

    new loop testing partial success

--- getTotalX.js
+++ getTotalX.js
@@ -9,36 +9,50 @@
 
 function getTotalX(a, b) {
   let sortedB = b.sort((a,b) => a - b);
+  let sortedA = a.sort((a,b) => a - b);
   let sortedBFactors = [];
   let finalFactors;
 
-  for (let i = 2; i <= Math.floor(sortedB[0]/2); i++ ) {
-    if (sortedB[0]%i === 0) {
-      sortedBFactors.push(i);
+  // another soltion: the least in b will be the iterated range.
+  // loop range set the min element value from b. find all the factors for a.
+  // elimiate above from not factors for b.
+  // find those factors between the Max of a and min of b, include both of them.
+
+  for (let j = sortedA[a.length-1]; j <= sortedB[0]; j++) {
+
+    for (let i = 0; i < a.length; i++) {
+      if (j%a[i] ===0) {
+        sortedBFactors.push(j);     // find a away to push one j.
+      }
     }
   }
-  sortedBFactors.push(sortedB[0]);
 
-  // filter them by divid other b elements.
-  // remove the element from sortedBFactors if they can not be divided by any other b elements.
+  // console.log(sortedBFactors)
 
   sortedB.forEach(el => {
     sortedBFactors = sortedBFactors.filter(factor => el%factor === 0)
-    console.log(sortedBFactors);
   })
 
+
+  finalFactors = sortedBFactors.filter((v,i) => sortedBFactors.indexOf(v) ===i);
+  console.log(finalFactors)
+  return finalFactors.length;
+
+
   // filter factors elements can be divided by a's element.
-  finalFactors = sortedBFactors.filter(el => {
-    for (let i = 0; i < a.length; i++) {
-      if (el%a[i]!==0) {
-        return false;
-      }
-    }
-    return true;
-  })
+  // finalFactors = sortedBFactors.filter(el => {
+  //   for (let i = 0; i < a.length; i++) {
+  //     if (el%a[i]!==0) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // })
 
   // console.log(sortedBFactors, finalFactors)
   // return finalFactors.length;
 }
 
-// console.log(getTotalX([8,4], [16,32,96]))
+console.log(getTotalX([2,4], [16,32,96]))
+console.log(getTotalX([3,4], [24, 48]))
+// getTotalX([2,6], [24,36])
