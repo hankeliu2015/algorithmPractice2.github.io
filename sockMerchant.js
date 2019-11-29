f9e2c27c06c61991f6538c53fafe6cd51951875c
github username <github email address>
2019-11-28 23:42:17 -0500

    testing passed

--- sockMerchant.js
+++ sockMerchant.js
@@ -1,6 +1,22 @@
-function sockMerchant(n, ar) {
+// first element equal to another element, splice them and add counter.
 
-  console.log(ar)
+
+function sockMerchant(n, ar) {
+  let counter = 0
+  let arrayLength = ar.length;
+  ar.forEach((el, index) => {
+    // compare el with the rest of elements, if anyone equal, splice the element from array.
+    for (let i = index +1 ; i < arrayLength; i++) {
+      if (el === ar[i]) {
+        counter++;
+        ar.splice(i,1);
+        arrayLength--;
+        break;
+      }
+    }
+  })
+
+  return counter;
 }
 
 console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
