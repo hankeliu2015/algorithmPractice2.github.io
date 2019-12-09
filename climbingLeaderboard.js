70d8dca24be91ebe0cfb0905550dfc93153adf42
github username <github email address>
2019-12-08 21:45:22 -0500

    solution 2 still having issues

--- climbingLeaderboard.js
+++ climbingLeaderboard.js
@@ -4,40 +4,36 @@
 // create a method to get ride of the duplicate.
 
 
-function climbingLeaderboard(scores, alice) {
-  let uniqueScores = scores.filter((el, index) => {
-    return scores.indexOf(el) === index;
-  });
-
-  let rankArray = alice.map((el, index) => {
-    let scoresWithAlice = uniqueScores
-    scoresWithAlice.push(el)
-    let sortedScores = scoresWithAlice.sort((a, b) => b-a)
-    return (sortedScores.indexOf(el) + 1);
-  })
-  return rankArray
-}
+// function climbingLeaderboard(scores, alice) {
+//   let uniqueScores = scores.filter((el, index) => {
+//     return scores.indexOf(el) === index;
+//   });
+//
+//   let rankArray = alice.map((el, index) => {
+//     let scoresWithAlice = uniqueScores
+//     scoresWithAlice.push(el)
+//     let sortedScores = scoresWithAlice.sort((a, b) => b-a)
+//     return (sortedScores.indexOf(el) + 1);
+//   })
+//   return rankArray
+// }
 
 // solution 2:
 // compare the score with the array. As long as matched, return the index value.
 // remove the duplicate before that index.
+function climbingLeaderboard(scores, alice) {
+  let rankArray = [];
+  alice.forEach(el => {
 
-// function climbingLeaderboard(scores, alice) {
-//
-//   let rankArray = [];
-//
-//   alice.forEach(el => {
-//
-//     rank = scores.map((score, index) => {
-//       if  (score < el) {
-//         return index + 1;
-//       }
-//     })
-//
-//     rankArray.push(rank);
-//   })
-//
-//   return rankArray
-// }
+    rank = scores.map((score, index) => {
+      if  (score < el) {
+        return index + 1;
+      }
+    })
+
+    rankArray.push(rank);
+  })
+  return rankArray
+}
 //
 // console.log(climbingLeaderboard([100,100,50,40,40,20,10], [2,25,50,120]))
