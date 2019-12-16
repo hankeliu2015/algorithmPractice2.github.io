43232cbd2b0d3b769e0a0c5fc698874ebb41c6b9
github username <github email address>
2019-12-16 16:14:37 -0500

    hurdleRace passed

---
+++ hurdleRace.js
@@ -0,0 +1,21 @@
+//find the largest number from the array
+// minus the k, return the value.
+
+
+function hurdleRace(k, height) {
+  let max = 0;
+  for (let i = 0; i < height.length; i ++) {
+    if ( max < height[i]) {
+      max = height[i];
+    }
+  }
+  if (k >= max) {
+    return 0;
+  } else {
+    return max - k;
+  }
+
+}
+
+console.log(hurdleRace(7, [2,5,4,5,2,]))
+console.log(hurdleRace(4, [1,6,3,5,2]))
