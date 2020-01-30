96c39f6a88be798d71a88faa47e901565994ede4
Hank Liu <hanke.liu@gmail.com>
2020-01-29 23:13:09 -0500

    create jQuery scritp and seperate html page

--- javascript/postJQueryTest1.js
+++ javascript/postJQueryTest1.js
@@ -1,35 +1,46 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
 $(() => {
-  bindClickHandlers();
+  console.log( "ready again!" );
 })
+// $(() => {
+//   bindClickHandlers();
+// })
 
-const bindClickHandlers = () => {
-  $('.all_post').on('click', (e) =>{
-    e.preventDefault();
-    fetch('/posts.json')
-    .then((res) => res.json)
-    .then(posts =>
-      $('#app-container').html('')
-      posts.forEach((post) => {
-        let newPost = new Post(post)
-        let postHtml = newPost.formatIndex()
-        $('app-container').append(postHtml)
-      })
-    )
-  })
-
-
-}
-
-function Post(post) {
-  this.id = post.id
-  this.title = post.title
-  this.content = post.Content
-  this.user = post.User
-  this.comments = post.comments
-}
-
-Post.prototype.formatIndex = function(){
-  let postHtml = `
-    <a href="/posts/${this.id}" class="show_link"><h1>${this.title}<h1></a>
-  `
-}
+// const bindClickHandlers = () => {
+//   $('.all_post').on('click', (e) =>{
+//     e.preventDefault();
+//     fetch('/posts.json')
+//     .then((res) => res.json)
+//     .then(posts =>
+//       $('#app-container').html('')
+//       posts.forEach((post) => {
+//         let newPost = new Post(post)
+//         let postHtml = newPost.formatIndex()
+//         $('app-container').append(postHtml)
+//       })
+//     )
+//   })
+//
+//   $(".show_link").on('click', (e) => {
+//     e.preventDefault();
+//     fetch(`/posts/json`)
+//   })
+// }
+//
+// function Post(post) {
+//   this.id = post.id
+//   this.title = post.title
+//   this.content = post.Content
+//   this.user = post.User
+//   this.comments = post.comments
+// }
+//
+// Post.prototype.formatIndex = function(){
+//   let postHtml = `
+//     <a href="/posts/${this.id}" class="show_link"><h1>${this.title}<h1></a>
+//   `
+// }
