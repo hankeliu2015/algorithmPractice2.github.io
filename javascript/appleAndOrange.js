fc9619891de539fe434aba8999b065ff63631272
Hank Liu <hanke.liu@gmail.com>
2020-02-07 14:08:14 -0500

    refactor syntax, remove map function

--- javascript/appleAndOrange.js
+++ javascript/appleAndOrange.js
@@ -3,27 +3,27 @@
 // console log the array length, which are true elements.
 
 
-// function countApplesAndOranges(s, t, a, b, apples, oranges) {
-//
-//   let applesArray = apples.map(el => {
-//     if (el + a >= s && el + a <= t) {
-//       return true;
-//     }
-//   }).filter(el => el === true)
-//
-//   let orangesArray = oranges.map(el => {
-//     if (el + b >= s && el + b <= t) {
-//       return true;
-//     }
-//   }).filter(el => el === true)
-//
-//   console.log(applesArray.length);
-//   console.log(orangesArray.length); // output formate request to print twice.
-//
-// }
-//
-// console.log(countApplesAndOranges(7, 11, 4, 12,[2, 3, -4], [3,-2,-4]));
-// console.log(countApplesAndOranges(7, 11, 5, 15,[-2, 2, 1], [5, -6]));
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+
+  let applesArray = apples.filter(el => {
+    if (el + a >= s && el + a <= t) {
+      return true;
+    }
+  })
+
+  let orangesArray = oranges.filter(el => {
+    if (el + b >= s && el + b <= t) {
+      return true;
+    }
+  })
+
+  console.log(applesArray.length);
+  console.log(orangesArray.length); // output formate request to print twice.
+
+}
+
+console.log(countApplesAndOranges(7, 11, 4, 12,[2, 3, -4], [3,-2,-4]));
+console.log(countApplesAndOranges(7, 11, 5, 15,[-2, 2, 1], [5, -6]));
 
 // solution 2: user a counter instead of filter.
 // function countApplesAndOranges(s, t, a, b, apples, oranges) {
