6b1ea3b6c0e28a66e8010c2c63a90f1b3ce08811
Hank Liu <hanke.liu@gmail.com>
2020-02-07 12:22:21 -0500

    comment out function and variable

--- javascript/testReduce.js
+++ javascript/testReduce.js
@@ -1,23 +1,23 @@
-const array1 = [1, 2, 3, 4, 5];
+// const array1 = [1, 2, 3, 4, 5];
 // const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 // console.log(array1.reduce((acc, curr) => acc + curr))
 // console.log(array1.reduce((acc, curr) => acc + curr, 5))
 
-console.log(array1.reduce((acc, curr) => {
-
-  console.log("acc;", acc + curr)
-  console.log("curr;", curr)
-  return acc + curr;
-}, 0))
+// console.log(array1.reduce((acc, curr) => {
+//
+//   console.log("acc;", acc + curr)
+//   console.log("curr;", curr)
+//   return acc + curr;
+// }, 0))
 
 // map reduce solution
 
-let maxCallback = ( acc, cur ) => Math.max( acc.x, cur.x )
-let maxCallback2 = ( max, cur ) => Math.max( max, cur )
-
-let mapReduceSolution = [ { x: 22 }, { x: 42 } ].map( el => el.x )
-                        .reduce( maxCallback2, -Infinity )
+// let maxCallback = ( acc, cur ) => Math.max( acc.x, cur.x )
+// let maxCallback2 = ( max, cur ) => Math.max( max, cur )
+//
+// let mapReduceSolution = [ { x: 22 }, { x: 42 } ].map( el => el.x )
+//                         .reduce( maxCallback2, -Infinity )
 
 
 // console.log(mapReduceSolution)
