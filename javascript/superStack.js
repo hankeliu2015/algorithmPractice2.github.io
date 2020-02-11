d9f4a2534fe9d35988adc2deab7d686e3063a688
Hank Liu <hanke.liu@gmail.com>
2020-02-11 11:43:16 -0500

    test new stack

--- javascript/superStack.js
+++ javascript/superStack.js
@@ -1,68 +1,78 @@
-let Stack1 = function() {
-  this.count = 0;
-  this.storage = {}
-}
-
-Stack1.prototype.push = function(value) {
-  this.storage[this.count] = value;
-  this.count++;
-}
-
-Stack1.prototype.pop = function() {
-  if (this.count === 0 ) {
-    return undefined;
-  }
-
-  this.count --;
-  var result = this.storage[this.count];
-  delete this.storage[this.count];
-  return result;
-}
-
-Stack1.prototype.size = function() {
-  return this.count;
-}
-
-class Stack2 {
-  constructor(){
-    this.count = 0;
-    this.storage = {}
-  }
-
-  push(value) {
-    this.storage[this.count] = value;
-    this.count++;
-  }
-
-  pop() {
-    if (this.count === 0 ) {
-      return undefined;
-    }
-    this.count --;
-    var result = this.storage[this.count];
-    delete this.storage[this.count];
-    return result;
-  }
-
-  size() {
-    return this.count;
-  }
-}
+function Stack( {
+  let items = [];
+  let top = 0; 
+})
 
 
-let stackTest1 = new Stack1();
-stackTest1.push(1);
-stackTest1.push(2);
-console.log(stackTest1.size());
-stackTest1.pop();
-console.log(stackTest1.size());
 
-let stackTest2 = new Stack1();
-stackTest2.push(1);
-stackTest2.push(2);
-console.log(stackTest2.size());
-stackTest2.pop();
-console.log(stackTest2.size());
+
+// let Stack1 = function() {
+//   this.count = 0;
+//   this.storage = {}
+// }
+//
+// Stack1.prototype.push = function(value) {
+//   this.storage[this.count] = value;
+//   this.count++;
+// }
+//
+// Stack1.prototype.pop = function() {
+//   if (this.count === 0 ) {
+//     return undefined;
+//   }
+//
+//   this.count --;
+//   var result = this.storage[this.count];  //store the current storage property
+//   delete this.storage[this.count];        // delete the original storage property
+//   return result;                          // return the updated storage property
+// }
+//
+// Stack1.prototype.size = function() {
+//   return this.count;
+// }
+//
+// class Stack2 {
+//   constructor(){
+//     this.count = 0;
+//     this.storage = {}
+//   }
+//
+//   push(value) {
+//     this.storage[this.count] = value;
+//     this.count++;
+//   }
+//
+//   pop() {
+//     if (this.count === 0 ) {
+//       return undefined;
+//     }
+//     this.count --;
+//     var result = this.storage[this.count];
+//     debugger
+//     delete this.storage[this.count];
+//     return result;
+//   }
+//
+//   size() {
+//     return this.count;
+//   }
+//
+// }
+
+// let stackTest1 = new Stack1();
+// stackTest1.push(1);
+// stackTest1.push(2);
+// stackTest1.push(3);
+// console.log(stackTest1.size());
+// console.log(stackTest1.pop());
+// console.log(stackTest1.size());
+
+// let stackTest2 = new Stack1();
+// stackTest2.push(1);
+// stackTest2.push(2);
+// console.log(stackTest2.size());
+// stackTest2.pop();
+// console.log(stackTest2.size());
 
 
 
