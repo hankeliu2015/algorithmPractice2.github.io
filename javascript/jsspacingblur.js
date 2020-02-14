680cf8473b36122f7f067ce5601da304f7e8ed90
Hank Liu <hanke.liu@gmail.com>
2020-02-14 10:54:20 -0500

    format update

--- javascript/jsspacingblur.js
+++ javascript/jsspacingblur.js
@@ -1,9 +1,6 @@
 const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate() {
-  // console.log(this.dataset)   // input element value attribute
-
-  // need setup suffice for each of the input.
 
   let suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
