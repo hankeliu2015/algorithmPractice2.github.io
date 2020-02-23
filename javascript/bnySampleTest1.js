6a10e29c469ee125e1171744b3091d8e5bfc4034
Hank Liu <hanke.liu@gmail.com>
2020-02-22 23:19:54 -0500

    create a temp sql comamnd record file

--- javascript/bnySampleTest1.js
+++ javascript/bnySampleTest1.js
@@ -8,3 +8,8 @@
 //// if divive by 3, it move to divid by 5. if divid by 5 also true, return f and b.
 //// if not divided by 5, it is fizz.
 //// if not divided by 3, it will jump to the second : , continue the conditions.
+
+
+// Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+
+// SELECT * FROM city WHERE population > 100000 AND countrycode = "USA"
---
+++ javascript/sqlCommandRecord.js
@@ -0,0 +1,3 @@
+// Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+
+// SELECT * FROM city WHERE population > 100000 AND countrycode = "USA"
