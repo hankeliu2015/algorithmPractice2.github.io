ff42101f759fe1a31a7139aa1ac8b00b9b08e9ab
Hank Liu <hanke.liu@gmail.com>
2020-02-22 22:15:46 -0500

    code test

---
+++ javascript/bnySampleTest1.js
@@ -0,0 +1,10 @@
+// var i = 1,
+// 	f = 'Fizz',
+// 	b = 'Buzz',
+// 	out = '';
+// for (; i <= 100; i++) {
+// 	out = !(i % 3) ?  !(i % 5)? f+b : f : !(i % 5)? b : i;
+
+//// if divive by 3, it move to divid by 5. if divid by 5 also true, return f and b.
+//// if not divided by 5, it is fizz.
+//// if not divided by 3, it will jump to the second : , continue the conditions.
