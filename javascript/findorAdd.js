24e8202bbfb0c2dc8e8979256300c48a0e8b5c37
Hank Liu <hanke.liu@gmail.com>
2020-02-23 23:32:51 -0500

    comment out sql testing syntax and comment out binary search testing syntaz

--- javascript/findorAdd.js
+++ javascript/findorAdd.js
@@ -1,33 +1,33 @@
-// function findOrAdd(rootNode, newNode){
-//   let currentNode = rootNode					 //current node changing with recursion // might not need this line.
-//   if(newNode.data < rootNode.data){
-//       currentNode = rootNode.left
-//       if(currentNode){                  // left has value
-//         findOrAdd(currentNode, newNode)			//go to the child level, check left and right
-//       } else {                          // left has not value
-//         rootNode.left = newNode          // eventally end here. base case.
-//       }
-//   } else if(newNode.data > rootNode.data) {
-//     currentNode = rootNode.right
-//     if(currentNode){
-//       findOrAdd(currentNode, newNode)
-//     } else {
-//       rootNode.right = newNode          //// eventally end here. base case.
-//     }
-//   }
-// }
+function findOrAdd(rootNode, newNode){
+  let currentNode = rootNode					 //current node changing with recursion // might not need this line.
+  if(newNode.data < rootNode.data){
+      currentNode = rootNode.left
+      if(currentNode){                  // left has value
+        findOrAdd(currentNode, newNode)			//go to the child level, check left and right
+      } else {                          // left has not value
+        rootNode.left = newNode          // eventally end here. base case.
+      }
+  } else if(newNode.data > rootNode.data) {
+    currentNode = rootNode.right
+    if(currentNode){
+      findOrAdd(currentNode, newNode)
+    } else {
+      rootNode.right = newNode          //// eventally end here. base case.
+    }
+  }
+}
 
 
-// // testing
-// let newNode = {data: 3, left: null, right: null};
-// let rootNode = {data: 6,
-//                 right: {data: 8, left: null, right: null},
-//                 left: {data: 1,
-//                   right: {data: 4, right: null, left: null},
-//                   left: null}
-//                 };
+// testing
+let newNode = {data: 3, left: null, right: null};
+let rootNode = {data: 6,
+                right: {data: 8, left: null, right: null},
+                left: {data: 1,
+                  right: {data: 4, right: null, left: null},
+                  left: null}
+                };
 
-// findOrAdd(rootNode, newNode);
-// console.log(rootNode);
-// console.log(rootNode.left.right.left);
-// console.log(newNode);
+findOrAdd(rootNode, newNode);
+console.log(rootNode);
+console.log(rootNode.left.right.left);
+console.log(newNode);
--- javascript/sqlCommandRecord.js
+++ javascript/sqlCommandRecord.js
@@ -11,5 +11,3 @@
 // SELECT city, state FROM station;
 
 // Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
-
-SELECT city FROM station
