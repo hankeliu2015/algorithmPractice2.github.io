f1625f780b47e51af93b21572fceb4a946ddce62
Hank Liu <hanke.liu@gmail.com>
2020-02-25 12:57:30 -0500

    testing distance Matrix function

--- javascript/bnySampleTest1.js
+++ javascript/bnySampleTest1.js
@@ -8,3 +8,28 @@
 //// if divive by 3, it move to divid by 5. if divid by 5 also true, return f and b.
 //// if not divided by 5, it is fizz.
 //// if not divided by 3, it will jump to the second : , continue the conditions.
+
+// 6. distance matrix
+function distanceMatrix(arr) {
+  // nest loop between each element and the rest of the element.
+  // set condition if elements equal, calculate distance and add to counter
+  let matrixArr = []
+
+  for(i=0; i<arr.length; i++) {
+
+    matrixArr[i] = 0;
+
+    for(j=0; j<arr.length; j++) {
+
+      let currentMatrix = 0;
+      // if both elements are equal and not the same one
+      if (arr[i] === arr[j] && i !==j) {
+        currentMatrix = Math.abs(i - j);
+        matrixArr[i] += currentMatrix;
+      }
+    }
+  }
+  return matrixArr;
+}
+
+console.log(distanceMatrix([1,2,1,1,2,3]))
