c22eb049a4116e1334c61288fd58ab5160a7f9b5
Hank Liu <hanke.liu@gmail.com>
2020-03-04 23:00:39 -0500

    basic variable and data type

--- javascript/pythonBasic.py
+++ javascript/pythonBasic.py
@@ -1,1 +1,27 @@
-# print("this is a python test")
+
+# print('Welcome to Python 101!2')
+# print('Welcome to Python 101!')
+#
+# print("Create hammer")
+# print("Create hammer")
+# print("Use hammer")
+
+# failed_subjects=2.45
+# name='John'
+# # Boolean True or False
+# a="it's"
+# b='it\'s'
+# print('Dear Mrs Badger')
+# print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+# print(name + '  will need to redo ' + failed_subjects + '  courses.')
+# name="Eric"
+# print(name + '  is doing well in geography.')
+# print(type('hello'))
+# print(type(1))
+# print(type(1.64))
+# print(type(True))
+
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
