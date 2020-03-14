ac79129694aca5bd4a33d12ad86e75f33325bdd6
Hank Liu <hanke.liu@gmail.com>
2020-03-13 20:45:46 -0400

    facebook code test

---
+++ javascript/facebook.js
@@ -0,0 +1,25 @@
+// function findNumber(arr, k) {
+//
+//   let j = "NO"
+//   for(let i = 0; i < arr.length; i ++) {
+//     if (k === arr[i]) {
+//       j = "YES";
+//       break;
+//     }
+//   }
+//   return j;
+// }
+//
+// console.log(findNumber([1,2,3,4,5], 6))
+
+// function oddNumbers(l, r) {
+//   let oddNums = [];
+//   for (let i = l; i <= r; i ++) {
+//     if ( i % 2 !== 0) {
+//       oddNums.push(i);
+//     }
+//   }
+//   return oddNums;
+// }
+//
+// console.log(oddNumbers(2,7));
