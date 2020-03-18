186d9427d41a240013ccadbedf1dcc8ffdf860d0
Hank Liu <hanke.liu@gmail.com>
2020-03-18 10:54:11 -0400

    refactor anagrams function

--- javascript/facebook.js
+++ javascript/facebook.js
@@ -1,72 +1,111 @@
 // //both strings has same size and same chars.
 // //remove each string taht is an anagram of an earlier string, then return the remianing array in sorted order.
+//
+
+//// anagrams solution | iterate through anagrams, find and remove anagrams. sort out the element left by alphabetically.
 
 function funWithAnagrams(text) {
-    let textObj = {}
-    textObj = text.reduce((acc, el) => {
-      acc[el] = el;
-      return acc
-    }, {})
 
-    let objArr = [];
     for (let i = 0; i < text.length ; i++) {
+      // the last element will have no anagrams. stop it compare to the last + 1 element.
       if ( !text[i + 1]) {
-          let lastEl = text[i].split('').sort().join('');
-          objArr[i] = {};
-          objArr[i][lastEl] = [text[i]];
           break;
         }
       for(let j = i + 1; j < text.length; j++) {
-        let textA = text[i].split('').sort().join('');
-        let textB = text[j].split('').sort().join('');
-        // debugger
-
-        if (text[i].length === text[j].length && textA === textB) {
-          if (!objArr[i]) {
-            objArr[i] = {};
-            objArr[i][textA] = [text[i]]
-            objArr[i][textA].push(text[j])
+        let currentElSortChars = text[i].split('').sort().join('');
+        let nextELSortChars = text[j].split('').sort().join('');
+        // there is a matched anagram, remove that from orignal array and reset the index backward.
+        if (text[i].length === text[j].length && currentElSortChars === nextELSortChars) {
+          if (!anagramArr[i]) {
             text.splice(j, 1);
             j -=1;
           } else {
-              objArr[i][textA].push(text[j]);
-              text.splice(j, 1);
-              j -=1;
+            text.splice(j, 1);
+            j -=1;
           }
-
         }
-
-        if (!objArr[i]) {
-          objArr[i] = {};
-          objArr[i][textA] = [text[i]];
-        }
-      } //outer loop
+        // for element without any anagrams
+      } //end of outer loop
     }
-// debugger
-    // for (const key in textObj) {
-    //   let currentSortedValue = textObj[key].split("").sort().join("");
-    //   textObj[key] = currentSortedValue
-    //   if (!textObj2[currentSortedValue]) {
-    //     textObj2[currentSortedValue] = [];
-    //     textObj2[currentSortedValue].push(textObj[key])
-    //   } else {
-    //     textObj2[currentSortedValue].push(textObj[key])
-    //   }
-    // }
-    // let combinedArr = []
-    // for (const key in textObj2) {
-    //   let single = textObj2[key].sort()
-    //   console.log(single)
-    // }
-    //
-    // return text;
-    console.log(objArr)
-}
+    return text.sort();
+  }
+
+// anagrams | array inside array to group anagrams, remove anagrams and sort the final array.
+
+// function funWithAnagrams(text) {
 //
-console.log(funWithAnagrams(["code", "doce", "ecod", "framer", "frame"]))
-// console.log(funWithAnagrams(["code", "aaagmnrs", "anagrams", "doce"]))
+//     let anagramArr = [];
+//     for (let i = 0; i < text.length ; i++) {
+//       // the last element will have no anagrams. stop it compare to the last + 1 element.
+//       if ( !text[i + 1]) {
+//           let lastEl = text[i].split('').sort().join('');
+//           anagramArr[i] = [];
+//           anagramArr[i] = [text[i]];
+//           break;
+//         }
+//       for(let j = i + 1; j < text.length; j++) {
+//         let textA = text[i].split('').sort().join('');
+//         let textB = text[j].split('').sort().join('');
+//         // there is a matched anagram, remove that from orignal array and reset the index backward.
+//         if (text[i].length === text[j].length && textA === textB) {
+//           if (!anagramArr[i]) {
+//             anagramArr[i] = [];
+//             anagramArr[i].push(text[i]);
+//             anagramArr[i].push(text[j]);
+//             text.splice(j, 1);
+//             j -=1;
+//           } else {
+//               anagramArr[i].push(text[j]);
+//               text.splice(j, 1);
+//               j -=1;
+//           }
+//         }
+//         // for element without any anagrams
+//         if (!anagramArr[i]) {
+//           anagramArr[i] = [];
+//           anagramArr[i] = [text[i]];
+//         }
+//       } //end of outer loop
+//     }
+//     ////iterate anagramArr and remove anagrams.
+//     uniqueArr = anagramArr.map(el => {
+//       // reset el's length to one.
+//       el.length = 1;
+//       return el;
+//     }).reduce((a,b) => a.concat(b), []).sort()
+//     return(uniqueArr);
+//   }
+
+  console.log(funWithAnagrams(["code", "doce", "ecod", "framer", "frame"]))
+  console.log(funWithAnagrams(["code", "aaagmnrs", "anagrams", "doce"]))
+  console.log(funWithAnagrams(["code", "aaagmnrs", "anagrams", "doce", "aaagmnsr", "anagarms"]))
+
 
 // 1. Artisan Martians:
+// // Daniel Dawson's example:
+// function stickers_for(phrase) {
+//   // keep track of letter frequencies
+//   const freq = {};
+//   let maxFreq = 0;
+//   // iterate through string
+//   for (const char of phrase) {
+//     if (char === ' ') continue; // skip spaces
+//     if (char in freq) {
+//       freq[char]++;
+//     } else {
+//       freq[char] = 1;
+//     }
+//     // account for second 'a' in 'instagram'
+//     const currentFreq = char === 'a' ? Math.ceil(freq[char] / 2) : freq[char];
+//     // update max
+//     if (currentFreq > maxFreq) {
+//       maxFreq = currentFreq;
+//     }
+//   }
+//   return maxFreq;
+// }
+
+//// mysolution 1
 //
 // function stickers_for(phrase) {
 //   let phraseArr = phrase.split("").filter(el => el != " ")
@@ -92,7 +131,7 @@ console.log(funWithAnagrams(["code", "doce", "ecod", "framer", "frame"]))
 //   return phraseObj[maxCharKey];
 // }
 
-// // function with Object.keys().Map
+// // function with Object.keys().Map - solution 2
 
 // function stickers_for(phrase) {
 //   let phraseArr = phrase.split("").filter(el => el != " ")
@@ -115,15 +154,15 @@ console.log(funWithAnagrams(["code", "doce", "ecod", "framer", "frame"]))
 //   })
 //   return Math.max(...phraseFrq)
 // }
-//
+
+
 // // "instagram"
 // console.log(stickers_for("artisan martins"))
 // console.log(stickers_for("taming giant gnats"))
 // console.log(stickers_for("tiara"))
 // console.log(stickers_for("i n s t a g r a m"))
 
-
-// test code before test.
+// sample test .
 // function findNumber(arr, k) {
 //
 //   let j = "NO"
@@ -149,27 +188,3 @@ console.log(funWithAnagrams(["code", "doce", "ecod", "framer", "frame"]))
 // }
 //
 // console.log(oddNumbers(2,7));
-
-// // Daniel Dawson's example:
-
-// function stickers_for(phrase) {
-//   // keep track of letter frequencies
-//   const freq = {};
-//   let maxFreq = 0;
-//   // iterate through string
-//   for (const char of phrase) {
-//     if (char === ' ') continue; // skip spaces
-//     if (char in freq) {
-//       freq[char]++;
-//     } else {
-//       freq[char] = 1;
-//     }
-//     // account for second 'a' in 'instagram'
-//     const currentFreq = char === 'a' ? Math.ceil(freq[char] / 2) : freq[char];
-//     // update max
-//     if (currentFreq > maxFreq) {
-//       maxFreq = currentFreq;
-//     }
-//   }
-//   return maxFreq;
-// }
