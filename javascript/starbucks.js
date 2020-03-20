e42ed026ea1178f97a945470f6b4bfc18f76053c
Hank Liu <hanke.liu@gmail.com>
2020-03-19 23:42:09 -0400

    comment out console.log

---
+++ javascript/starbucks.js
@@ -0,0 +1,6 @@
+// Given a string of all lowercase letters, find the lexicographically largest substring. Assume valid input.
+// function LexicographicalMaxString(str) {
+//
+// }
+//
+// console.log(LexicographicalMaxString("ababaa"))
