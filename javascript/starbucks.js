f8d40f806e9908132392d34dba564218efac2f01
Hank Liu <hanke.liu@gmail.com>
2020-03-22 21:59:57 -0400

    lca test passed

--- javascript/starbucks.js
+++ javascript/starbucks.js
@@ -5,6 +5,8 @@
 // //
 // // console.log(LexicographicalMaxString("ababaa"))
 //
+// // Lowest Common Acestor
+
 // lowestCommonAncestor = function(root, p, q) {
 //   if (!root)
 //     return null;
@@ -22,16 +24,41 @@
 //   else
 //     return right;
 // };
-//
-// // //testing
-// let n1 = { data: 5, left: null, right: null};
-// let n2 = {data: 2, left: null, right: null};
-//
-// let rootNode3 = {data: 6,
-//                   right: {data: 8, left: null, right: null},
-//                   left:  {data: 1,
-//                     right: {data: 4, right: { data: 5, left: null, right: null}, left: {data: 2, left: null, right: null}},
-//                     left: null}
-//                 };
-//
-// console.log(lowestCommonAncestor(rootNode3, n1, n2))
+
+function lowestCommonAncestor(root, p, q) {
+  if (p.data < root.data && q.data < root.data){
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.data > root.data && q.data > root.data) {
+    return lowestCommonAncestor(root.right, p, q)
+  } else {
+    return root;
+  }
+}
+
+// //testing
+//   6
+//  / \
+// 1   8
+//  \
+//   4
+//  / \
+// 2   5
+
+let n5 = { data: 5, left: null, right: null};
+let n4 =  {data: 4, right: { data: 5, left: null, right: null}, left: {data: 2, left: null, right: null}};
+let n2 = {data: 2, left: null, right: null};
+let n1 = {data: 1,
+  right: {data: 4, right: { data: 5, left: null, right: null}, left: {data: 2, left: null, right: null}},
+  left: null};
+let n8 = {data: 8, left: null, right: null}
+
+
+let rootNode3 = {data: 6,
+                  right: {data: 8, left: null, right: null},
+                  left:  {data: 1,
+                    right: {data: 4, right: { data: 5, left: null, right: null}, left: {data: 2, left: null, right: null}},
+                    left: null}
+                };
+
+console.log(lowestCommonAncestor(rootNode3, n1, n2))
+console.log(lowestCommonAncestor(rootNode3, n2, n8))
