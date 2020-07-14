e4f86f3cfed63a5b5e3f0064d20a5982627e685d
Hank Liu <hanke.liu@gmail.com>
2020-07-14 14:49:37 -0400

    closure test

--- javascript/closureTest.js
+++ javascript/closureTest.js
@@ -1,8 +1,18 @@
-var add = (function () {
-  var counter = 0;
-  return function () {counter += 1; return counter}
-})();
-
-add();
-add();
-add();
+// var add = (function () {
+//   var counter = 0;
+//   return function () {counter += 1; return counter}
+// })();   //self invoking function
+//
+// add();
+// add();
+//
+//
+// function counterTest() {
+//   let counter = 0;
+//   return function () {counter += 1; return counter}
+// }
+//
+// let addTest = counterTest()
+//
+// addTest();
+// addTest();
