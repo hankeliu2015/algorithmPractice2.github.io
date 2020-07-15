b6041e57df44c836af3c6af3cae381f9f0614ede
Hank Liu <hanke.liu@gmail.com>
2020-07-15 10:50:32 -0400

    test findoddoccurence

--- javascript/findOddOccurence.js
+++ javascript/findOddOccurence.js
@@ -1,24 +1,52 @@
 
-function findOddOccurence(arr) {
-  var arrHash = {};
-  for (var i = 0; i < arr.length; i++) {
-    if (arrHash[arr[i]] !== undefined) {
-      arrHash[arr[i]] ++
-    } else {
-      arrHash[arr[i]] = 1
-    }
-  }
-  console.log(arrHash)
-  let counter = 0
-  for ( var key in arrHash) {
+// function findOddOccurence(arr) {
+//   var arrHash = {};
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arrHash[arr[i]] !== undefined) {
+//       arrHash[arr[i]] ++
+//     } else {
+//       arrHash[arr[i]] = 1
+//     }
+//   }
+//   console.log(arrHash)
+//   let counter = 0
+//   for ( var key in arrHash) {
+//
+//     if (parseInt(key) % 2 !== 0) {
+//       console.log("inside condition: ", key);
+//       counter += arrHash[key]
+//     }
+//   }
+//   return counter;
+// }
 
-    if (parseInt(key) % 2 !== 0) {
-      console.log("inside condition: ", key);
-      counter += arrHash[key]
+
+function findOddOccurence(arr) {
+  // change the array into object , element as the key and value as the occurance
+  // if the element equal to a key, the value will increment. if not, create a key value pair.
+  let obj = {};
+  for(i = 0; i < arr.length; i++) {
+    if(!obj[arr[i]] === false){
+      obj[arr[i]] += 1;
+      // debugger;
+    }else {
+      obj[arr[i]] = 1;
     }
   }
+
+  //set up a count for the obj, count the odd key
+
+  let counter = 0;
+  for(const key in obj) {
+    if(parseInt(key) % 2 !== 0){
+      // debugger;
+      counter += obj[key];
+    }
+  }
+
   return counter;
 }
 
-// const input = [1,2,4,2,2,4,1,4,3,3,3];
-// console.log(findOddOccurence(input))
+//test
+const input = [1,2,4,2,2,4,1,4,3,3,3];
+console.log(findOddOccurence(input))
