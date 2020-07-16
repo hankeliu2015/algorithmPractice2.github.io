ca414a05b95c3401142082ecce9ea75ad3aaf456
Hank Liu <hanke.liu@gmail.com>
2020-07-16 10:39:03 -0400

    fomat update

--- javascript/countingPairs.js
+++ javascript/countingPairs.js
@@ -1,3 +1,35 @@
+//retest
+// function countingPairs(num, k) {
+//   let matchedPairs = [];
+//   let uniquePair = false;
+//
+//   for(let i = 0; i < num.length; i++){
+//     for (let j = i + 1; j < num.length; j++ ){
+//       if (((num[i] + k === num[j]) || (num[j] + k === num[i])) && (matchedPairs.length == 0)){
+//           matchedPairs.push([num[i], num[j]]);
+//         } else if (((num[i] + k === num[j]) || (num[j] + k === num[i])) && (matchedPairs.length !== 0)) {
+//
+//           uniquePair = matchedPairs.map(el => {
+//             if ((el[0] === num[i]) && (el[1] === num[j])){
+//               return false;
+//             } else if ((el[1] === num[i]) && (el[0] === num[j])){
+//               return false;
+//             } else {
+//               return true;
+//             }
+//           }).reduce((a,b) => a && b)
+//
+//           if (uniquePair) {
+//             matchedPairs.push([num[i], num[j]]);
+//           }
+//         }
+//     }
+//   }
+//
+//   console.log(matchedPairs.length)
+// }
+
+// original test
 // function countingPairs(numbers, k) {
 //
 //   let counter = 0;
@@ -32,40 +64,9 @@
 //   console.log(pairArr.length);
 // }
 
-//retest
-function countingPairs(num, k) {
-  let matchedPairs = [];
-  let uniquePair = false;
-
-  for(let i = 0; i < num.length; i++){
-    for (let j = i + 1; j < num.length; j++ ){
-      if (((num[i] + k === num[j]) || (num[j] + k === num[i])) && (matchedPairs.length == 0)){
-          matchedPairs.push([num[i], num[j]]);
-        } else if (((num[i] + k === num[j]) || (num[j] + k === num[i])) && (matchedPairs.length !== 0)) {
-
-          uniquePair = matchedPairs.map(el => {
-            if ((el[0] === num[i]) && (el[1] === num[j])){
-              return false;
-            } else if ((el[1] === num[i]) && (el[0] === num[j])){
-              return false;
-            } else {
-              return true;
-            }
-          }).reduce((a,b) => a && b)
-
-          if (uniquePair) {
-            matchedPairs.push([num[i], num[j]]);
-          }
-        }
-    }
-  }
-
-  console.log(matchedPairs.length)
-}
-
-countingPairs([ 1, 2], 1)
-countingPairs([ 1, 1, 2, 2], 1)
-countingPairs([ 1, 1, 2, 2, 3, 3, 4, 4], 1)
-countingPairs([ 1, 1, 2, 2, 3, 3, 4, 4, 5, 6], 1)
-countingPairs([ 6,5,4,4,3,3,2,2,1,1], 1)
+// countingPairs([ 1, 2], 1)
+// countingPairs([ 1, 1, 2, 2], 1)
+// countingPairs([ 1, 1, 2, 2, 3, 3, 4, 4], 1)
+// countingPairs([ 1, 1, 2, 2, 3, 3, 4, 4, 5, 6], 1)
+// countingPairs([ 6,5,4,4,3,3,2,2,1,1], 1)
 // 3
