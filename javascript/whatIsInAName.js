1e412f113218e4f08d88628185baa90a5794917c
Hank Liu <hanke.liu@gmail.com>
2020-07-17 22:53:43 -0400

    reduce test done

--- javascript/whatIsInAName.js
+++ javascript/whatIsInAName.js
@@ -6,18 +6,16 @@
 // also same element object mush has the same value.
 
 // follow by a condition, if the 1st and 2nd has the same key, and value are the same, also 2nd argument has less keys than the 1st argument, return true.
-
+//
 // function whatIsInAName(collection, source) {
 //   let matchedArray = collection.filter(obj => {
 //
 //     for (const key in obj ) {
-//
 //       for (const k in source) {
 //         if ( !obj.hasOwnProperty(k) || source[k] !== obj[k]) {
 //           return false;    //not has the key or has the same key with fifferent value.
 //         }
 //       }
-//
 //       if (obj[key] === source[key] && Object.keys(obj).length >= Object.keys(source).length ) {
 //         return true;     //has the same key value pair. also the collection element can have more objects.
 //       }
@@ -27,12 +25,34 @@
 //   return matchedArray;
 // }
 
+//re-test // logic is better than above.
+
+// function whatIsInAName(collection, source){
+//   let matchedData = collection.filter(collectData => {
+//     for(let collectKey in collectData) {
+//       for(let sourceKey in source){
+//
+//         if(!collectData.hasOwnProperty(sourceKey)) {
+//           return false;
+//         }else if( collectData[sourceKey] !== source[sourceKey]){
+//           return false;
+//         }else if (Object.keys(source).length > Object.keys(collectData).length){
+//           return false;
+//         }
+//       }
+//       return true;
+//     }
+//   })
+//   return matchedData;
+// }
+
+
 // console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 //
 // console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }));
 //
 // console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }))
-
+//
 // console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}))
 
 
@@ -56,5 +76,18 @@
 //
 //     });
 //
+//   return data;
+// }
+
+//retest
+
+// function whatIsInAName(collection, source){
+//   let srcKeys = Object.keys(source);
+//
+//   let data = collection.filter(el => {
+//     return srcKeys.map(srcKey => {
+//       return el.hasOwnProperty(srcKey) && el[srcKey] === source[srcKey];
+//     }).reduce((a,b) => a && b)
+//   })
 //   return data;
 // }
