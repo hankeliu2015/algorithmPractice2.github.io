ad1eaf97ce4fe989585c08177149675abdd6d316
Hank Liu <hanke.liu@gmail.com>
2020-07-19 16:24:45 -0400

    retest bst tree print inorder

--- javascript/bstTreeInOrder.js
+++ javascript/bstTreeInOrder.js
@@ -1,5 +1,6 @@
-//
+//print out the nodes of a tree in order
 // // // printNodeWithoutLeft | node.left's value less than node.
+
 // function printNodeWithoutLeft(currentNode){
 //   if(currentNode.left){
 //     printNodeWithoutLeft(currentNode.left)
@@ -9,25 +10,69 @@
 //     printNodeWithoutLeft(currentNode.right)
 //   }
 // }
-// //
-// //
+//
+//
 // let rootNode2 = {data: 6,
 //                   right: {data: 8, left: null, right: null},
 //                   left:  {data: 1,
 //                     right: {data: 4, right: { data: 5, left: null, right: null}, left: {data: 2, left: null, right: null}},
 //                     left: null}
 //                 };
-//
+
+// rootNode1 in chart
+// binary search tree following order: 4, 1, 2, 5, 6, 3.
+  // 4        4      4         4             4              4
+  //          /      /        /   \         /   \          /   \
+  //         1      1        1     5       1     5        1     5
+  //                  \       \             \     \        \     \
+  //                  2        2            2     6        2     6
+  //                                                        \
+  //                                                         3
+
+// let rootNode1 = {
+//   data: 4,
+//   left: {
+//     data: 1,
+//     left: null,
+//     right: {
+//       data: 2,
+//       left: null,
+//       right: {
+//         data: 3,
+//         left: null,
+//         right: null
+//       }
+//     }
+//   },
+//   right: {
+//     data: 5,
+//     left: null,
+//     right: {
+//       data: 6,
+//       left: null,
+//       right: null
+//     }
+//   }
+// }
+
+// printNodeWithoutLeft(rootNode1);
 // printNodeWithoutLeft(rootNode2);
+
+//retest
+// printInOrder(rootNode1);
+// printInOrder(rootNode2);
 //
-// /*
+// function printInOrder(node){
+//   let currentNode = 0;
+//   if(node.left){
+//     currentNode = node.left
+//     printInOrder(currentNode);
+//   }
+//   console.log(node.data);
 //
-// console output:
-// 1
-// 2
-// 4
-// 5
-// 6
-// 8
+//   if(node.right) {
+//     currentNode = node.right;
+//     printInOrder(currentNode);
+//   }
 //
-// */
+// }
