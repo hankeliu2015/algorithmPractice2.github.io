a5c78b4ee2cb59a36ff5c5ae286e9da2577b7729
Hank Liu <hanke.liu@gmail.com>
2020-07-19 21:45:41 -0400

    retest bst tree print inorder

--- javascript/bstfindOrAdd.js
+++ javascript/bstfindOrAdd.js
@@ -19,44 +19,46 @@
 //
 //
 // //testing
-let newNode = {data: 3, left: null, right: null};
-let rootNode = {data: 6,
-                right: {data: 8, left: null, right: null},
-                left: {data: 1,
-                  right: {data: 4, right: null, left: null},
-                  left: null}
-                };
-
-findOrAdd(rootNode, newNode);
+// let newNode = {data: 3, left: null, right: null};
+// let rootNode = {data: 6,
+//                 right: {data: 8, left: null, right: null},
+//                 left: {data: 1,
+//                   right: {data: 4, right: null, left: null},
+//                   left: null}
+//                 };
+//
+// findOrAdd(rootNode, newNode);
 // console.log(rootNode);
 // console.log(rootNode.left.right.left);
 // console.log(newNode);
 
-function findOrAdd(rootNode, newNode){
-  let currentNode = 0;
 
-
-  if(newNode.data > rootNode.data ){
-    if(rootNode.right === null){
-      rootNode.right = newNode;
-
-      return true;
-    }else{
-      currentNode = rootNode.right;
-      findOrAdd(currentNode, newNode);
-    }
-  }
-
-  if(newNode.data < rootNode.data){
-    if(rootNode.left === null){
-      rootNode.left = newNode;
-
-      return true;
-    }else{
-      currentNode = rootNode.left;
-      findOrAdd(currentNode, newNode);
-    }
-  }
-
-  return console.log(rootNode);
-}
+//retest
+// function findOrAdd(rootNode, newNode){
+//   let currentNode = 0;
+//
+//
+//   if(newNode.data > rootNode.data ){
+//     if(rootNode.right === null){
+//       rootNode.right = newNode;
+//
+//       return true;
+//     }else{
+//       currentNode = rootNode.right;
+//       findOrAdd(currentNode, newNode);
+//     }
+//   }
+//
+//   if(newNode.data < rootNode.data){
+//     if(rootNode.left === null){
+//       rootNode.left = newNode;
+//
+//       return true;
+//     }else{
+//       currentNode = rootNode.left;
+//       findOrAdd(currentNode, newNode);
+//     }
+//   }
+//
+//   return console.log(rootNode);
+// }
