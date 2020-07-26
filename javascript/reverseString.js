ba8a62e017b430f38904646a8b1cc2c0c35c1994
Hank Liu <hanke.liu@gmail.com>
2020-07-25 22:49:58 -0400

    test reverse string

--- javascript/reverseString.js
+++ javascript/reverseString.js
@@ -1,17 +1,57 @@
+// let myString = 'Algorithm';
+//
+// // Set 'myString' equal to a substring of itself minus the last letter...
+// myString = myString.substring(0, myString.length - 1) +
+// // ...and then add the last letter back:
+// myString[myString.length - 1];
+//
+// // myString still contains 'Algorithm'!
+// console.log(myString);
+// // => "Algorithm"
+
+//// print a string
+
+// function printString(str) {
+//   let subString;
+//   if (str.length === 0){
+//     return true;
+//   } else {
+//     console.log(str[0]);
+//     subString = str.substring(1, str.length);
+//     printString(subString);
+//   }
+// }
+//
+// console.log(printString("string"))
+
 // let newString = ''
 
-function reverseString(myString) {
-  if (myString.length < 2) {
-    return myString;				//base case. Return value will show.
+// function reverseString(myString) {
+//   if (myString.length < 2) {
+//     return myString;				//base case. Return value will show.
+//   } else {
+//     // console.log(myString)	// show the substring. following return value won’t show because the recursive
+//     // newString = newString + myString + '|';
+//     // debugger
+//     return reverseString(myString.substring(1)) + myString[0];
+// //put the first character to the end, the substring break up with the first element until the last one.
+//   }
+// }
+
+////retest ververstring
+
+function reverseString(str) {
+  //base case
+  if ( str.length < 2) {
+    return str;
   } else {
-    // console.log(myString)	// show the substring. following return value won’t show because the recursive
-    // newString = newString + myString + '|';
-    // debugger
-    return reverseString(myString.substring(1)) + myString[0];
-//put the first character to the end, the substring break up with the first element until the last one.
+    let char = str.substring(str.length - 1)
+    let subStr = str.substring(0, str.length -1)
+    return char.concat(reverseString(subStr));
   }
 }
 
+console.log(reverseString("lissa"))
 // let newString = reverseString('pizza')
 // console.log(newString)
 // console.log(reverseString('pizza'))
--- javascript/sumUpTo.js
+++ javascript/sumUpTo.js
@@ -1,34 +1,39 @@
-// let sum = 0
+//// sayDownFrom 5 function
+// function sayDownFrom(n) {
+//   //base case n = 0, stop the recursion. return 0.
+//   if (n === 0) {
+//     return true;
+//   } else {
+//     console.log(n);
+//     n--;
+//   }
+//   sayDownFrom(n);
+// }
 //
+// console.log(sayDownFrom(5));
+
+// let sum = 0
+
 // function sumUpTo(n){
-// console.log(n)						//must have console.log to show the value.
+//   console.log(n)						//must have console.log to show the value.
 //   if(n > 1){
 //     return sumUpTo(n - 1) + n;
 //   } else {
 //     return 1;						//base case
 //   }
-//   return sum;
+//   // return sum;
 // }
-//
-// console.log(sumUpTo(5));
 
+// console.log(sumUpTo(5));
 
 // retest
 
+// function sumUpTo(n) {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   return n + sumUpTo(n-1); // base caase sumUpTo(n-1) will return 1,
+// }
 
-function sumUpTo(n) {
-  //base case
-
-  if (n === 1) {
-    console.log(n);
-    return n;
-  } else {
-    // debugger
-    console.log(n);
-    n = n + sumUpTo(n-1);
-  }
-
-  return n;
-}
-
-console.log(sumUpTo(5))
+// console.log(sumUpTo(5))
+// console.log(sumUpTo(10))
