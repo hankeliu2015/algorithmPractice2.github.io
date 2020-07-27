a1f580f5bcc3f9069938bf9434a839a6c6ed6f1a
Hank Liu <hanke.liu@gmail.com>
2020-07-27 14:23:20 -0400

    test string permutations

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -24,30 +24,30 @@
 //   return isAPalindrome(chopString(str))  		// recursuive function take in the string, which removed 1st and last char
 // }
 
-function isAPalindrome(str) {
+// function isAPalindrome(str) {
   //reverse the string and compare if it is equal
   // base case is the string length is 0
-  let revStr;
-  let reverseString = (str) => {
-    // base case when str length equal to 0
-    if (str.length === 1){
-      return str;
-    } else {
-      // use substring as recursive function argument
-      // use the last char concat the recursive function with substring as argument
-      let subStr = str.substring(0, str.length - 1)
-      return str[str.length -1].concat(reverseString(subStr))
-    }
-  }
-
-  if (reverseString(str) === str) {
-    return true;
-  } else {
-    return false;
-  }
-
-}
-
-console.log(isAPalindrome("cattac"))
-console.log(isAPalindrome("12345"))
-console.log(isAPalindrome("catac"))
+//   let revStr;
+//   let reverseString = (str) => {
+//     // base case when str length equal to 0
+//     if (str.length === 1){
+//       return str;   // return the origin first char.
+//     } else {
+//       // use substring as recursive function argument
+//       // use the last char concat the recursive function with substring as argument
+//       let subStr = str.substring(0, str.length - 1)
+//       return str[str.length -1].concat(reverseString(subStr))
+//     }
+//   }
+//
+//   if (reverseString(str) === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+//
+// }
+//
+// console.log(isAPalindrome("cattac"))
+// console.log(isAPalindrome("12345"))
+// console.log(isAPalindrome("catac"))
--- javascript/stringPermutation.js
+++ javascript/stringPermutation.js
@@ -12,14 +12,14 @@
 //       if (string.length === 1)
 //       {
 //         results.push(string);
-//         return results;
+//         return results;          //base case
 //       }
 //
 //       for (var i = 0; i < string.length; i++)
 //       {
 //         var firstChar = string[i];    //1
 //         var otherChar = string.substring(0, i) + string.substring(i + 1); //23
-//         var otherPermutations = getPermutations(otherChar);   // after percursive returned 3, it will concatenate with 2 in the j loop
+//         var otherPermutations = getPermutations(otherChar);   // after recursive returned 3, it will concatenate with 2 in the j loop
 //                                                               // next i loop of 23, 2 will concatenate with 3.
 //         for (var j = 0; j < otherPermutations.length; j++) {
 //           results.push(firstChar + otherPermutations[j]);   // 2 + 3
@@ -37,7 +37,30 @@
 //     // console.log(permutation);
 //     console.log("Total permutation: "+permutation1.length);
 //     console.log(permutation1);
-// //
+
+//retest | recursive method
+
+function findPerms(str) {
+  if (str.length === 1) return [str]
+
+  let all = []
+  for (let i = 0; i < str.length; i++) {
+    const currentLetter = str[i]
+    const remainingLetters = str.slice(0,i) + str.slice(i+1)
+    const permsOfRemainingLetters = findPerms(remainingLetters)
+    // console.log(permsOfRemainingLetters);
+
+    permsOfRemainingLetters.forEach(subPerm => {
+      console.log(subPerm);
+      all.push(currentLetter + subPerm)
+    })
+  }
+  return all
+}
+
+console.log(findPerms("TWO"))
+
+
 // // // none recursive solution:
 // // //
 // // // function stringPermutation(str) {
@@ -68,3 +91,30 @@
 // //
 // // // let str = 'cats'
 // // //
+
+// rerest | failed
+
+// function stringPermutation(str) {
+//   // push re-ordered strings to array
+//   let reOrder = [];
+//   let tempStr = str;
+//   let tempChar;
+//   // outter loop pick one element to swap with each of the rest element of inner loop.
+//   for (let i = 0; i < str.length; i++) {
+//     // this part of the str stay the same for inner loop
+//     temChar = str[i];
+//     for (let j = i + 1; j < str.length - (i + 1); j++) {
+//       // swap i char with the rest of the chars and push the new string to reOrder.
+//       // swap the index of the string
+//       // afterStr = str.slice(j + 1, str.length)
+//       // tempStr = preStr + str[j] + afterStr;
+//       // use over write instead of concat, char[i] become char[j]
+//       // the rest of the string are the same.
+//       // repalce char[i] with char[j] without change the str.
+//       tempStr.replaceAt(i, )
+//
+//     }
+//   }
+// }
+//
+// console.log(stringPermutation("cats"))
