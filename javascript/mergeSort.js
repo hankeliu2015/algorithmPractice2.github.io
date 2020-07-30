2f74cf1920e08e5fdb12beb083c0bf7a2e49aa7f
Hank Liu <hanke.liu@gmail.com>
2020-07-29 23:55:02 -0400

    consolidate files and reformat comments

--- javascript/mergeSort.js
+++ javascript/mergeSort.js
@@ -2,51 +2,51 @@
 // let firstHalf =  [1, 2, 6, 7, 12]
 // let secondHalf =  [3, 4, 5, 9, 11]
 //
-function findMinAndRemove(firstHalf, secondHalf){
-  let minfirstHalf = firstHalf[0]
-  let minsecondHalf = secondHalf[0]
-
-  if(minfirstHalf < minsecondHalf){
-    return firstHalf.shift()
-  } else {
-    return secondHalf.shift()
-  }
-}
-
-function merge(firstHalf, secondHalf){
-  let sorted = []
-  let currentMin;
-
-  while(firstHalf.length != 0 && secondHalf.length != 0){
-    let currentMin = findMinAndRemove(firstHalf, secondHalf)
-    sorted.push(currentMin)
-  }
-  return sorted.concat(firstHalf).concat(secondHalf)
-}
-
-// console.log(merge(firstHalf, secondHalf))
-
-function mergeSort(array){
-    let midpoint = array.length/2;
-    let firstHalf = array.slice(0, midpoint);
-    let sortedArr;
-    let secondHalf = array.slice(midpoint, array.length)
-    if(array.length < 2){
-      console.log(`firstHalf: ${firstHalf}, secondHalf: ${secondHalf}`);
-      // debugger
-      return array    //base case relturn value when length === 2;
-    } else {
-      sortedArr = merge(mergeSort(firstHalf), mergeSort(secondHalf)) ////recursive here
-    }
-    console.log(`sorted array: ${sortedArr}`);
-    return sortedArr;   // must have a return here.
-  }
+// function findMinAndRemove(firstHalf, secondHalf){
+//   let minfirstHalf = firstHalf[0]
+//   let minsecondHalf = secondHalf[0]
+//
+//   if(minfirstHalf < minsecondHalf){
+//     return firstHalf.shift()
+//   } else {
+//     return secondHalf.shift()
+//   }
+// }
+//
+// function merge(firstHalf, secondHalf){
+//   let sorted = []
+//   let currentMin;
+//
+//   while(firstHalf.length != 0 && secondHalf.length != 0){
+//     let currentMin = findMinAndRemove(firstHalf, secondHalf)
+//     sorted.push(currentMin)
+//   }
+//   return sorted.concat(firstHalf).concat(secondHalf)
+// }
+//
+// // console.log(merge(firstHalf, secondHalf))
+//
+// function mergeSort(array){
+//     let midpoint = array.length/2;
+//     let firstHalf = array.slice(0, midpoint);
+//     let sortedArr;
+//     let secondHalf = array.slice(midpoint, array.length)
+//     if(array.length < 2){
+//       console.log(`firstHalf: ${firstHalf}, secondHalf: ${secondHalf}`);
+//       // debugger
+//       return array    //base case relturn value when length === 2;
+//     } else {
+//       sortedArr = merge(mergeSort(firstHalf), mergeSort(secondHalf)) ////recursive here
+//     }
+//     console.log(`sorted array: ${sortedArr}`);
+//     return sortedArr;   // must have a return here.
+//   }
 
 // let array =  [2, 1, 7, 6, 8, 3, 4, 5]
 // let array =  [2, 1, 7, 6]
 // console.log(mergeSort(array))
 //
-// //// BigO cheatsheet solution test ( skip for now ) // skip for now . 
+// //// BigO cheatsheet solution test ( skip for now ) // skip for now .
 //
 // let array = [12, 11, 15, 10, 9, 1, 2, 3, 13, 14, 4, 5, 6, 7, 8]
 // mergeSort(array, 0, array.length - 1)
