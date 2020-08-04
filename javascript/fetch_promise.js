c142ab26c80868ccacc1b36227eb5a05a783302d
Hank Liu <hanke.liu@gmail.com>
2020-08-03 23:19:16 -0400

    test promise

---
+++ javascript/fetch_promise.js
@@ -0,0 +1,21 @@
+//// using fetch on MDN
+
+
+
+
+//// using promise on MDN
+
+// function successCallback(result) {
+//   console.log("Audio file ready at URL: " + result);
+// }
+//
+// function failureCallback(error) {
+//   console.error("Error generating audio file: " + error);
+// }
+//
+// createAudioFileAsync(audioSettings, successCallback, failureCallback);
+//
+// createAudioFileAsync(audioSettings).then(successCallback, failureCallback);
+//
+// const promise = createAudioFileAsync(audioSettings);
+// promise.then(successCallback, failureCallback);
