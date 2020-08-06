ed53485704ff05f24ac0e8ad577b59a2a3a7d7f5
Hank Liu <hanke.liu@gmail.com>
2020-08-05 23:46:40 -0400

    functional test

--- javascript/fetch_promise.js
+++ javascript/fetch_promise.js
@@ -1,3 +1,163 @@
+// closure
+
+const add = (a, b) => a + b;
+const add10 = partialApply(add, 10);
+// add10(5);
+
+const partialApply = (fn, ...fixedArgs) => {
+  return function (...remainingArgs) {
+    return fn.apply(this, fixedArgs.concat(remainingArgs));
+  };
+};
+
+
+function test() {
+  const msg = 'partialApply() should partially apply functions'
+
+  const add = (a, b) => a + b;
+
+  const add10 = partialApply(add, 10);
+
+
+  const actual = add10(5);
+  const expected = 15;
+
+  test.equal(actual, expected, msg);
+};
+
+test();
+
+
+// functional programing
+
+// const x = {
+//   val: 2
+// }
+//
+// // const x1 = () => x.val += 1;
+// // const x2 = () => x.val *= 2;
+//
+// const x1 = (x) => Object.assign({}, x, {val: x.val + 1})
+// const x2 = (x) => Object.assign({}, x, {val: x.val * 2})
+//
+// // x1();
+// // x2();
+// // console.log(x.val);
+//
+// console.log(x1(x2(x)).val);
+//
+// const y = {
+//   val: 2
+// };
+//
+// x2(y);
+// x1(y);
+//
+// console.log(x1(x2(y)).val)
+
+//
+// const a = Object.freeze({
+//   foo: { greeting: 'Hello' },
+//   bar: 'world',
+//   baz: '!'
+// });
+//
+// const b = Object.create({
+//   foo: { greeting: 'Hello' },
+//   bar: 'world',
+//   baz: '!'
+// });
+//
+// a.foo.greeting = 'Goodbye';
+// b.foo2 = 'Goodbye2';
+//
+// // console.log(`${ a.foo.greeting }, ${ a.bar }${a.baz}`);
+// console.log(b.foo2);
+
+
+
+// //// Asynchronous on Netflix
+//
+//
+// function play(movieId, cancelButton, callback) {
+//
+//   let movieTicket, playError;
+//
+//   // track do I have movie ticket and player loaded.
+//   let tryFinish = function() {
+//     if (playError) {
+//       callback(null, playError)
+//     }
+//     else if ( movieTicket && player.intialized) {
+//       callback(null, ticket);
+//     }
+//   };
+//   // tracking the error
+//   cancelButton.addEventListener("click", function() {playError = "cancelled"}
+//     if (!player.intialized) {
+//       player.init(function(error) {
+//         playError = error;
+//         tryFinish();
+//     }
+//     authorizeMovie(function(error, ticket) {
+//       playError = error;
+//       movieTicket = ticket;
+//       tryFinish();
+//     });
+//   });
+//
+// //// observer pattern
+//
+// document.addEventListener("mosemove", function next(e) {
+//   console.log(e);
+// })
+//
+// } // end of function
+//
+// //// top rated move collection
+//
+// var getTopRatedFilms = user =>
+//   user.videLists.
+//     map(videoList =>                                // return all the video list contains movies rated 5.0. array inside array.
+//         videoList.videos.
+//           filter(video => video.rating === 5.0)).   //filter return all movies which rated 5.0
+//           concatAll();                              // flatten arrays inside array
+// getTopRatedFilms(user).
+//   forEach(film => cosole.log(film));                // output all elements inside array.
+//
+// //// mouse drag collection
+//
+// var getElementDrags = elmt = >
+//   elemt.mouseDowns.
+//     map(mouseDown =>
+//       document.mouseMoves.
+//         takeUntil(document.mouseUps)).
+//     concatAll();
+//
+// getElementDrags(image).
+//   forEach(pos => image.position = pos);
+//
+//   //// event subscription
+//   //// subscribe
+//   var handle = (e) => console.log(e);
+//   document.addEventListener("mousemoves", handler);
+//   //// unsubscribe
+//   document.removeEventListener("mosemoves", handler);
+//
+// //// expanded Observable.forEach
+// // subscribe
+// var subscription =
+//   mouseMoves.forEach(
+//     //next data
+//     event => console.log(event),
+//     //error
+//     error => console.error(error),
+//     // completed
+//     () => console.log("done"));
+// // unsubscribe
+//
+// subscription.dispose();
+
 //// promise on Eric Elliott
 //// simple example
 // const wait = time => new Promise((resolve) => setTimeout(resolve, time));
@@ -13,7 +173,7 @@
 //   onFulfilled?: Function,
 //   onRejected?: Function
 // ) => Promise
-//
+//// example II
 // wait(200)
 //   // onFulfilled() can return a new promise, `x`
 //   .then(() => new Promise(res => res('foo')))
@@ -49,17 +209,17 @@
 
 //// ().then MDN
 
-var p1 = new Promise((resolve, reject) => {
-  resolve('Success!');
-  // or
-  // reject(new Error("Error!"));
-});
-
-p1.then(value => {
-  console.log(value); // Success!
-}, reason => {
-  console.error(reason); // Error!
-});
+// var p1 = new Promise((resolve, reject) => {
+//   resolve('Success!');
+//   // or
+//   // reject(new Error("Error!"));
+// });
+//
+// p1.then(value => {
+//   console.log(value); // Success!
+// }, reason => {
+//   console.error(reason); // Error!
+// });
 
 //// using promise on MDN
 
