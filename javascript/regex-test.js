dba00a0b119322d4662d3331ec14030bcaa99ac4
Hank Liu <hanke.liu@gmail.com>
2020-08-10 17:10:59 -0400

    regex testing

---
+++ javascript/regex-test.js
@@ -0,0 +1,56 @@
+// let petStr = " James has a pet cat."
+// let petRegex = /dog|cat|bird|fish/
+//
+// console.log(petRegex.test(petRegex))
+//
+// // use i flag to ignore case
+//
+// let myStr = 'feeCodeCamp';
+// let fccRegex = /codecamp/i
+//
+// console.log(fccRegex.test(myStr))
+
+// extract match
+
+// let extracgStr = 'Extract the word coding from this string'
+// let codingRegex = 'coding';
+// console.log(extracgStr.match(codingRegex))
+//
+// // find more than the first match
+//
+let twinkelStar = "Twinkle, twinkle, little star";
+// let starRegex = /twinkle/ig;
+// console.log(twinkelStar.match(starRegex))
+//
+// // wild card
+let unRegex = /.in/g;
+console.log(unRegex.test(twinkelStar));
+console.log(twinkelStar.match(unRegex));
+
+// multi match
+let quoteSample = 'Mississipp. ssss. Beaware of bugs in the above code 123456;'
+// let baRegex = /b[aeiou]g/;
+// let vowelRegrex = /[aeiou]/ig;
+// console.log(quoteSample.match(baRegex)[0])
+// console.log(quoteSample.match(vowelRegrex))
+
+// match a range of letter and a range of numbers
+// let adMatch = /[a-f]/ig;
+// let numCharMatch = /[a-f2-4]/ig;
+// console.log(quoteSample.match(adMatch))
+// console.log(quoteSample.match(numCharMatch))
+
+// negated charactor sets
+// let negatedMatch = /[^0-3aeiou]/ig;
+// console.log(quoteSample.match(negatedMatch))
+
+//match a char one or more times
+
+// let oneMoreMatch = /s+/ig;
+// console.log(quoteSample.match(oneMoreMatch
+
+// match chars that occur 0 or more times
+
+let chewieQuote = "Aaaaaaaaarrrgh! Aa A a";
+let chewieRegex = /Aa*/g;
+console.log(chewieQuote.match(chewieRegex))
