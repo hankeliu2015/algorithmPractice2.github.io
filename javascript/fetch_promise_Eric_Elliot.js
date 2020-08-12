546ee867c33d030dda51a42983e2630603c62da4
Hank Liu <hanke.liu@gmail.com>
2020-08-12 12:39:19 -0400

    reorgnize fetch and promise file

--- javascript/fetch_promise_Eric_Elliot.js
+++ javascript/fetch_promise_Eric_Elliot.js
@@ -1,3 +1,29 @@
+
+// const getSecret = (secret) => {
+//   return {
+//     get: () => secret
+//   };
+// };
+
+// test("Closure for object privacy.", assert => {
+//
+//   const msg = ".get() should have access to the closure.";
+//   const expected = 1;
+//   const obj = getSecret(1);
+//
+//   const actual = obj.get();
+//
+//   try {
+//     assert.ok(secret, 'This throws an error.');
+//   } catch (e) {
+//     assert.ok(true, `The secret var is only available
+//       to privileged methods.`);
+//   }
+//
+//   assert.equal(actual, expected, msg);
+//   assert.end();
+// })
+
 // function composition
 
 // const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
