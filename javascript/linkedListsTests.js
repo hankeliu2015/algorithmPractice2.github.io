9ba5ad17fb271d53b4dcc2d6d7c61999fae6fd1d
Hank Liu <hanke.liu@gmail.com>
2021-05-24 23:27:00 -0400

    more linked list testing

--- javascript/linkedListsTests.js
+++ javascript/linkedListsTests.js
@@ -1,15 +1,84 @@
 
 function printLinkedList(head) {
-// if the node next is true, , loop keep going and print the node data
-// if the node next is false, stop the loop
-  let node = head; 
-  if (!node.next) { console.log(node.data)}
 
-    // if node.next is 16, print 13, node change to 16. 
-    // if node is the last node, the node.next is null. 
+  let node = head; 
   while(node) {
-    console.log(node.data); // 16, print 16
-    node = node.next;       // 13, 13 next is null, it won't print 13 th eloop stopped. 
+    console.log(node.data); 
+    node = node.next;      
   }
 
+}
+
+class Node {
+  constructor(data, next = null) {
+      this.data = data; 
+      this.next = null; 
+  }
+}
+function insertNodeAtTail(head, data) {
+  let tmp = new Node(); tmp.data = data; tmp.next = null;
+
+  if(head == null) {
+      head = tmp;
+      return head;
+  } 
+  
+  let current = head;
+  while(current.next != null) {
+      current = current.next;
+  }
+  current.next = tmp;
+  return head;
+
+}
+
+function insertNodeAtHead(head, data) {
+  let newHead = new Node(data);
+  
+  if(!head) {
+      return newHead; 
+  }else {
+      newHead.next = head
+      return newHead;  
+  }
+}
+
+function insertNodeAtPosition(llist, data, position) {
+  const newNode = new Node(data);
+  if(!llist) {
+    return newNode; 
+  }else if (position === 0) {
+    newNode.next = llist; 
+    return newNode; 
+  }
+
+  let currentNode = llist;
+  let previousNode; 
+  let counter = 0; 
+
+  while(counter !== position) {
+    previousNode = currentNode; 
+    currentNode = currentNode.next; 
+    counter ++; 
+  }
+
+  previousNode.next = newNode; 
+  newNode.next = currentNode; 
+  return llist; 
+}
+
+function deleteNode(llist, position) {
+  // if the postoin is the head, return llist.next or null. 
+  if(position === 0) { return llist.next }; 
+  // need the previous node next and current next node. 
+  let counter = 0; 
+  let currentNode = llist; 
+  let previousNode; 
+  while(counter !== position){
+    previousNode = currentNode; 
+    currentNode = currentNode.next;
+    counter ++;
+  }
+  previousNode.next = currentNode.next; 
+  return llist; 
 }
