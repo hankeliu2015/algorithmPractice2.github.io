7d0dc26c9e2ebc9c741e615eb038bb108bd3522c
hankeliu2015 <hanke.liu@gmail.com>
2022-04-21 17:49:49 -0400

    start a new branch for js functional programming

---
+++ javascript/interview-walmart.js
@@ -0,0 +1,14 @@
+/* 
+
+Create a function named
+arity
+which takes a function as an argument and returns the number of defi ned arguments the function accepts.
+For example:
+If,
+const add = (a, b) => a + b
+const addOne = (a) => a + 1
+Then,
+arity(add) should return 2
+arity(addOne) should return 1
+
+*/
