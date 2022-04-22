dcaccf150c19ab79a6b643ad3a24e02025c798a9
hankeliu2015 <hanke.liu@gmail.com>
2022-04-21 21:55:30 -0400

    update questions

--- javascript/interview-walmart.js
+++ javascript/interview-walmart.js
@@ -1,8 +1,8 @@
 /* 
-
+18 
 Create a function named
 arity
-which takes a function as an argument and returns the number of defi ned arguments the function accepts.
+which takes a function as an argument and returns the number of defined arguments the function accepts.
 For example:
 If,
 const add = (a, b) => a + b
@@ -11,4 +11,76 @@ Then,
 arity(add) should return 2
 arity(addOne) should return 1
 
+*/
+
+/*
+Create a function called functionalize which takes a value and returns a function, which when executed, returns that value.
+For example:
+functionalize('a')() should return 'a'
+functionalize(false)() should return false
+*/
+
+function functionalize2(val) {
+    let returnVal = function(val) {
+        return val
+    }
+    return returnVal()
+    
+}
+
+/* 
+20
+Create a function named compose which accepts an unspecified number of single-argument functions as arguments and returns a function which executes those functions from left to right.
+For example:
+If,
+const addOne = (a) => a + 1;
+const multiplyByTwo = (b) => b * 2;
+Then,
+compose(addOne, multiplyByTwo)(3) should return 8.
+*/
+
+/*
+Create a function named setDefault which takes an argument of any value and returns a function, which when passed a truthy argument, returns that truthy argument, and when passed a falsy argument, returns the original argument passed to setDefault
+.
+For example:
+setDefault(72)(true) should return true
+setDefault('foobar')(false) should return 'foobar'
+*/
+
+/*
+Create a function named safelyTraverse which takes an object as the fi rst parameter and an array of strings as the second parameter. The function should return the value found after treating each string as a key traversing the object. If the path does not exist, the function should return undefi ned.
+For example:
+safelyTraverse({ fi rst: { second: 2 } }, ['fi rst', 'second']) should return 2
+safelyTraverse({}, ['a', 'b']) should return undefi ned
+*/
+
+/*
+Create a function named throwOn2 which accepts an integer as an argument and returns the integer if it is not 2. If it is 2, the function should throw an error with message "2 is not allowed."
+For example:
+throwOn2(3) should return 3
+throwOn2(2) should throw an error and error.message should be "2 is not allowed"
+Answer given:
+function throwOn2(int) {
+throw()
+}
+Words: 5
+*/
+
+/*
+Create a function named promisifyValue which takes an argument of any type and returns a Promise which resolves to that value.
+For example:
+await promisify(2) should return 2
+await promisify(3).then(val => val + 1) should return 4
+*/
+
+/*
+Create a function named promisifyFunction which takes a synchronous function
+fn with an unspecifi ed argument signature as an argument and returns a function with the same argument signature that returns a promise which resolves to the output of fn with passed arguments.
+For example:
+If,
+const add = (a, b) => a + b
+const multiplyByTwo = (c) => c * 2
+Then,
+await promisifyFunction(add)(1, 1) should return 2
+await promisifyFunction(multiplyByTwo)(3).then(val => val + 1) should return 7
 */
