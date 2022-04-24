119add8dbaacd4d2831a1711cdcddbf76a19c58b
hankeliu2015 <hanke.liu@gmail.com>
2022-04-23 23:56:09 -0400

    try catch function is passed

--- javascript/interview-walmart.js
+++ javascript/interview-walmart.js
@@ -313,11 +313,11 @@ function functionalize(val) {
 Create a function named compose which accepts an unspecified number of single-argument functions as arguments and returns a function which executes those functions from left to right.
 For example:
 If,
+should return 8.
+
+*/
 const addOne = (a) => a + 1;
 const multiplyByTwo = (b) => b * 2;
-Then,
-compose2(addOne, multiplyByTwo)(3) should return 8.
-*/
 
 const compose = (...fns) => (val) => {
     let result = fns.reduce((acc, currentFn) => {
@@ -325,6 +325,8 @@ const compose = (...fns) => (val) => {
     }, val)  
     return result
 }
+
+// console.log(compose(addOne, multiplyByTwo)(3)) 
 
 /*
 Create a function named setDefault which takes an argument of any value and returns a function, which when passed a truthy argument, returns that truthy argument, and when passed a falsy argument, returns the original argument passed to setDefault
@@ -334,28 +336,50 @@ setDefault(72)(true) should return true
 setDefault('foobar')(false) should return 'foobar'
 */
 
-const setDefault = function(val) {
-    // create a interanl function here. 
+const setDefault = (defaultV) => (val) => {
+    return  function innerFn() {
+        if (val === false) {
+            return defaultV;
+        } else {
+            return true;
+        }
+    }
 }
 
+console.log(setDefault(72)(true)()); 
+console.log(setDefault('foobar')(false)()); 
+
 /*
-Create a function named safelyTraverse which takes an object as the first parameter and an array of strings as the second parameter. The function should return the value found after treating each string as a key traversing the object. If the path does not exist, the function should return undefi ned.
+Create a function named safelyTraverse which takes an object as the first parameter and an array of strings as the second parameter. The function should return the value found after treating each string as a key traversing the object. If the path does not exist, the function should return undefined.
 For example:
-safelyTraverse({ fi rst: { second: 2 } }, ['fi rst', 'second']) should return 2
-safelyTraverse({}, ['a', 'b']) should return undefi ned
+safelyTraverse({ first: { second: 2 } }, ['first', 'second']) should return 2
+safelyTraverse({}, ['a', 'b']) should return undefined
 */
+
 
 /*
 Create a function named throwOn2 which accepts an integer as an argument and returns the integer if it is not 2. If it is 2, the function should throw an error with message "2 is not allowed."
 For example:
 throwOn2(3) should return 3
 throwOn2(2) should throw an error and error.message should be "2 is not allowed"
-Answer given:
-function throwOn2(int) {
-throw()
+
+const throwOn2 = function(int) {
+    try {
+        if(int === 2) {
+            throw "2 is not allowd"
+        } else {
+            return int;
+        }
+    }
+    catch(err) {
+        return err
+    }
 }
-Words: 5
+
+console.log(throwOn2(3))
+console.log(throwOn2(2))
 */
+
 
 /*
 Create a function named promisifyValue which takes an argument of any type and returns a Promise which resolves to that value.
